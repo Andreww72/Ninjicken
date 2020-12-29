f02f45333f01c8b5b8c6c929f823883e43d6f334
Andrew <litix@manjaro.modem>
2020-12-29 22:55:07 +1000

    Proper show board function, implement eval check

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
 #include <string>
-#include <map>
 #include "main.h"
-using namespace std;
+#include "eval.h"
 
 // TODO
     // Board representation and rules
@@ -11,87 +10,102 @@ using namespace std;
     // GUI interface
     // Opening book
     // 5 men endgame tablebase
-    // Parallel search
+    // Bitboard board representation
+    // Parallel search function
     // Ongoing improvement of search and evaluate
 
 int main() {
-    cout << "Welcome to Ninjicken\n";
-    cout << "Copyright 2020 Andrew Mather\n";
-    cout << "Use \"help\" to show commands\n";
-    cout << "Don't have too much fun!\n\n";
+    std::cout << "Welcome to Ninjicken\n";
+    std::cout << "Copyright 2020 Andrew Mather\n";
+    std::cout << "Use \"help\" to show commands\n";
+    std::cout << "Don't have too much fun!\n\n";
 
     // Initialise stuff
-    bool play = false;
-    bool playerTurn = true;
+    bool play{false};
+    bool playerTurn{true};
     int colour;
-    show_board();
-
-    // INIT BOARD
-    // INIT OPENING BOOK
-    // INIT TABLEBASES
+    Board gameBoard;
+    show_board(gameBoard);
 
     for (;;) {
-        // If computers turn run think
+        // Is it the computer's turn?
         if (play && !playerTurn) {
-            cout << "Computer's turn\n";
+            std::cout << "Computer's turn\n";
+
+            // Computer think and play move here
+
+            // End of turn pass over to player
+            playerTurn = true;
+            continue;
         }
 
         // If persons turn ask for input
-        string inputStr;
-        cout << "\nNinjicken: ";
-        cin >> inputStr;
+        std::string inputStr;
+        std::cout << "\nNinjicken: ";
+        std::cin >> inputStr;
 
         // Check if a command
         // Else parse entered move
         if (inputStr.compare("help") == 0) {
-            cout << "help - display help menu\n";
-            cout << "quit - quit program\n";
-            cout << "show - show board\n";
-            cout << "play - play game and select colour\n";
-            cout << "end - end game and reset board\n";
-            cout << "undo - undo prev move not implemented\n";
-            cout << "depth n - search depth not implemented\n";
-            cout << "time n - search time not implemented\n";
+            std::cout << "help - display help menu\n";
+            std::cout << "quit - quit program\n";
+            std::cout << "show - show board\n";
+            std::cout << "play - play game and select colour\n";
+            std::cout << "end - end game and reset board\n";
+            std::cout << "eval - evaluation score\n";
+            std::cout << "undo - undo prev move not implemented\n";
+            std::cout << "depth n - search depth not implemented\n";
+            std::cout << "time n - search time not implemented\n";
             continue;
         }
         if (inputStr.compare("quit") == 0) {
-            cout << "Quitting Ninjicken\n";
+            std::cout << "Quitting Ninjicken\n";
             break;
         }
         if (inputStr.compare("show") == 0 || inputStr.compare("s") == 0) {
-            show_board();
+            show_board(gameBoard);
             continue;
         }
         if (inputStr.compare("play") == 0) {
             if (play) {
-                cout << "Game already in progress\n";
+                std::cout << "Game already in progress\n";
                 continue;
             }
 
-            cout << "Beginning a game\n";
-            cout << "Select white (0) or black (1): ";
+            std::cout << "Beginning a game\n";
+            std::cout << "Select white (0) or black (1): ";
             int colourSelect;
-            cin >> colourSelect;
+            std::cin >> colourSelect;
 
             if (colourSelect == WHITE) {
                 colour = WHITE;
                 play = true;
-                cout << "Game beginning as white\n";
+                playerTurn = true;
+                std::cout << "Game beginning as white\n";
             } else if (colourSelect == BLACK) {
                 colour = BLACK;
                 play = true;
-                cout << "Game beginning as black\n";
+                playerTurn = false;
+                std::cout << "Game beginning as black\n";
             } else {
-                cout << "Invalid choice\n";
+                std::cout << "Invalid choice\n";
                 play = false;
             }
             continue;
         }
         if (inputStr.compare("end") == 0) {
-            cout << "Game state reset\n";
+            std::cout << "Game state reset\n";
             play = false;
-            // TODO: reset here
-            show_board();
+            // DESTROY BOARD OBJECT
+            // CREATE NEW BOARD OBJECT
+            show_board(gameBoard);
+            continue;
+        }
+        if (inputStr.compare("eval") == 0) {
+            int eval = evaluate(gameBoard);;
+            std::cout << "Evaluation: ";
+            std::cout << eval;
+            std::cout << "\n";
             continue;
         }
 
@@ -99,21 +113,41 @@ int main() {
         // Check game in progress and players turn
         // Move parsing
 
-        cout << "default";
+        std::cout << "default";
         break;
     }
 
-    // close a book file if open
     return 0;
 }
 
-void show_board() {
-    std::cout << "rnbqkbnr\n";
-    std::cout << "pppppppp\n";
-    std::cout << "........\n";
-    std::cout << "........\n";
-    std::cout << "........\n";
-    std::cout << "........\n";
-    std::cout << "PPPPPPPP\n";
-    std::cout << "RNBQKBNR\n";
+void show_board(Board gameBoard) {
+    
+    for (int i = 0; i < 8; i++) {
+        for (int j = 0; j < 8; j++) {
+            switch (gameBoard.board[i][j]) {
+                case EMPTY:
+                    std::cout << ".";
+                    break;
+                case PAWN:
+                    std::cout << "P";
+                    break;
+                case KNIGHT:
+                    std::cout << "N";
+                    break;
+                case BISHOP:
+                    std::cout << "B";
+                    break;
+                case ROOK:
+                    std::cout << "R";
+                    break;
+                case QUEEN:
+                    std::cout << "Q";
+                    break;
+                case KING:
+                    std::cout << "K";
+                    break;
+            }
+        }
+        std::cout << "\n";
+    }
 }
--- src/main.h
+++ src/main.h
@@ -1,4 +1,11 @@
-int WHITE = 0;
-int BLACK = 1;
+#ifndef MAIN_H
+#define MAIN_H
 
-void show_board();
+#include "board.h"
+
+const int WHITE = 0;
+const int BLACK = 1;
+
+void show_board(Board gameboard);
+
+#endif
